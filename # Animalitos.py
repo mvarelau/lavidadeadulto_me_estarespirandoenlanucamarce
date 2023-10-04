3743f14d2b4df21d7cccded37274ed96df9bce70
mvarelau <141885396+mvarelau@users.noreply.github.com>
2023-10-04 09:59:16 -0500

    Punto 3

---
+++ # Animalitos.py
@@ -0,0 +1,6 @@
+# Animalitos
+def carnita(N, M, K):
+    total: float= 6*N + 7*M + K
+    print(f"El total de kilos de carne es: {total}")
+
+carnita(((float(input("Ingrese la cantidad de gallinas: "))), (float(input("Ingrese la cantidad de gallos: "))), (float(input("Ingrese la cantidad de pollitos: ")))))
